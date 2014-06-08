c0a4bb6bde50bb5127306b005983c6dd3729701d
dragonshade <rxxxie@gmail.com>
2014-06-08 07:37:45 -0700

    chess ongoing

--- xml_parser.rb
+++
@@ -1,8 +0,0 @@
-
-#scan the string
-# if sees "<", indicates open tag
-# read until it hits >
-# push string between < > on stack
-# apppend stuff until it hits > 
-# start a new stack
-# append stuff to the stack , go back to line 3
