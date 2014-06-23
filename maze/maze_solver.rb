fda3345265cc15e0dcd349edd234865f84105f4c
dragonshade <rxxxie@gmail.com>
2014-06-23 16:46:54 -0700

    fixed indentation for maze

--- maze/maze_solver.rb
+++ maze/maze_solver.rb
@@ -9,62 +9,62 @@ class MazeSolver
 
 
   def initialize(filename, welcome = false)
-  	@memo = Hash.new(false)
-  	@maze = Maze.new(filename)
-  	greet if welcome
-  	@solution = []
+    @memo = Hash.new(false)
+    @maze = Maze.new(filename)
+    greet if welcome
+    @solution = []
   end
 
   def greet
-  	puts "Target Maze loaded..."
-  	@maze.pp(true)
+    puts "Target Maze loaded..."
+    @maze.pp(true)
   end
 
   def dfs_solver
-  	puts "Solving maze with Depth First Search...."
+    puts "Solving maze with Depth First Search...."
     solver("pop")
   end
 
   def bfs_solver
-  	puts "Solving maze with Breath First Search...."
+    puts "Solving maze with Breath First Search...."
     solver("shift")
   end
 
   def solver(method)
-  	@root = Node.new(nil, @maze.start)
-  	queue = [@root]
+    @root = Node.new(nil, @maze.start)
+    queue = [@root]
 
-  	while !queue.empty?
-  	  current_node = queue.send(method)
-  	  if current_node.pos == @maze.finish
-    		backtrack!(current_node)
-    		break
-  	  else
-    		expand_at!(current_node).each do |child_node|
-      		@memo[child_node.pos] = true
-      		queue << child_node
-    		end
-  	  end
-  	end
+    while !queue.empty?
+      current_node = queue.send(method)
+      if current_node.pos == @maze.finish
+        backtrack!(current_node)
+        break
+      else
+        expand_at!(current_node).each do |child_node|
+          @memo[child_node.pos] = true
+          queue << child_node
+        end
+      end
+    end
   end
 
   def clear
-  	@memo = Hash.new(false)
-  	@solution = []
+    @memo = Hash.new(false)
+    @solution = []
   end
 
   def backtrack!(finished_node)
-	current = finished_node
-	while current.pos != @maze.start
-	  current = current.parent
-	  @solution << current.pos 
-	end
-	#@solution.pop  #without overwritting the "S" in maze
+  current = finished_node
+  while current.pos != @maze.start
+    current = current.parent
+    @solution << current.pos 
+  end
+  #@solution.pop  #without overwritting the "S" in maze
   end
 
   def print_solution
-	duplicate = @maze.dup
-	(0...@solution.length).each do |i|
+  duplicate = @maze.dup
+  (0...@solution.length).each do |i|
     next if @solution[i+1].nil?
     delta = [@solution[i+1].first - @solution[i].first, 
              @solution[i+1].last - @solution[i].last]
@@ -78,26 +78,26 @@ class MazeSolver
     when [0, 1] #right
       duplicate[@solution[i]] = LEFT
     end
-	end
-	duplicate.pp
+  end
+  duplicate.pp
   end
 
   def expand_at!(node)
-	i, j = node.pos.first, node.pos.last
-	directions = [[i+1,j],[i-1,j],[i,j+1],[i,j-1]]
-	expandable = []
-	directions.each do |dir|
-	  if @maze.in_bound?(dir) && (not @maze.wall?(dir)) && (not in_memo?(dir))
-		child_node = Node.new(node,dir)
-		expandable << child_node
-		node.children << child_node
-	  end
+  i, j = node.pos.first, node.pos.last
+  directions = [[i+1,j],[i-1,j],[i,j+1],[i,j-1]]
+  expandable = []
+  directions.each do |dir|
+    if @maze.in_bound?(dir) && (not @maze.wall?(dir)) && (not in_memo?(dir))
+    child_node = Node.new(node,dir)
+    expandable << child_node
+    node.children << child_node
     end
-	return expandable
+    end
+  return expandable
   end
 
   def in_memo?(pos)
-	  return @memo[pos]
+    return @memo[pos]
   end
 end
 
@@ -107,13 +107,13 @@ class Node
 
   def initialize(parent,pos)
     @parent = parent
- 	  @pos = pos
-	  @children =[]
+    @pos = pos
+    @children =[]
   end
 
   def add_node(node)
-	  node.parent = self
-	  @children << node
+    node.parent = self
+    @children << node
   end
 end
 
@@ -122,31 +122,31 @@ class Maze
   attr_accessor :start, :finish, :maze
 
   def initialize(filename)
-  	@filename = filename
-  	@maze = File.readlines(@filename).map do |line|
-  	  line.chomp.split('')
-	  end
-	@maze.each_with_index do |array, row|
-	  array.each_with_index do |spot, col|
-  		if spot == 'S'
-  		  @start = [row,col]
-  		elsif spot == 'E'
-  		  @finish = [row,col]
-  		end
-	  end
-	end
+    @filename = filename
+    @maze = File.readlines(@filename).map do |line|
+      line.chomp.split('')
+    end
+  @maze.each_with_index do |array, row|
+    array.each_with_index do |spot, col|
+      if spot == 'S'
+        @start = [row,col]
+      elsif spot == 'E'
+        @finish = [row,col]
+      end
+    end
+  end
 
-  	@height = @maze.length
-  	@width = @maze[0].length
+    @height = @maze.length
+    @width = @maze[0].length
   end
 
   def dup
-  	duplicate = Maze.new(@filename)
-  	return duplicate
+    duplicate = Maze.new(@filename)
+    return duplicate
   end
 
   def [](pos)
-  	return @maze[pos.first][pos.last]
+    return @maze[pos.first][pos.last]
   end
 
   def []=(pos,val)
@@ -154,15 +154,15 @@ class Maze
   end
 
   def in_bound?(pos)
-	  (0...@height).cover?(pos.first) && (0...@width).cover?(pos.last)
+    (0...@height).cover?(pos.first) && (0...@width).cover?(pos.last)
   end
 
   def wall?(pos)
-	  self[pos] == '*'
+    self[pos] == '*'
   end
 
   def pp(descript = false)
-	  puts "Start from #{@start} to #{@finish}" if descript
+    puts "Start from #{@start} to #{@finish}" if descript
     @maze.each do |row|
       row.each do |grid|
         if grid == "*"
